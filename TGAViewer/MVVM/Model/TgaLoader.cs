674a62aa90960ca8c680eca42277d4ed9e7c4a82
2latemc <christoph.fagerer@gmx.ch>
2023-08-17 21:56:16 +0200

    Init

---
+++ TGAViewer/MVVM/Model/TgaLoader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using FreeImageAPI;
+
+namespace TGAViewer.MVVM.Model;
+
+public class TgaLoader {
+    public static BitmapSource? LoadAndDisplayTgaImage(string filePath) {
+        if (!File.Exists(filePath)) {
+            MessageBox.Show("File does not exist");
+            Environment.Exit(0);
+            return default;
+        }
+
+
+        // try
+        // {
+        //     FIBITMAP dib = FreeImage.Load(FREE_IMAGE_FORMAT.FIF_TARGA,filePath,FREE_IMAGE_LOAD_FLAGS.DEFAULT);
+        //
+        //     IntPtr bits = FreeImage.GetBits(dib);
+        //     var width = FreeImage.GetWidth(dib);
+        //     var height = FreeImage.GetHeight(dib);
+        //     var dpiX = FreeImage.GetDotsPerMeterX(dib);
+        //     var dpiY = FreeImage.GetDotsPerMeterY(dib);
+        //     var size = FreeImage.GetDIBSize(dib);
+        //     
+        //     BitmapSource bitmapSource = BitmapSource.Create(
+        //         (int)width,
+        //         (int)height,
+        //         dpiX,
+        //         dpiY,
+        //         PixelFormats.Bgr32, 
+        //         null,
+        //         bits,
+        //         (int)size *,
+        //         (int)(width * 4)); 
+        //     FreeImage.UnloadEx(ref dib);
+        //
+        //     return bitmapSource;
+        // } catch (Exception e)
+        // {
+        //     MessageBox.Show("Could not load file. " + e + " " + filePath);
+        //     Environment.Exit(0);
+        //     return default;
+        // }
+
+
+        FIBITMAP image = FreeImage.LoadEx(filePath);
+
+        var map = FreeImage.GetBitmap(image);
+        
+        BitmapSource bitmapSource =  Convert(map);
+        FreeImage.UnloadEx(ref image);
+        return bitmapSource;
+
+        // return new BitmapImage(new Uri(filePath, UriKind.RelativeOrAbsolute));
+    }
+    
+    
+    public static BitmapSource Convert(System.Drawing.Bitmap bitmap)
+    {
+        var bitmapData = bitmap.LockBits(
+            new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+            System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+        var bitmapSource = BitmapSource.Create(
+            bitmapData.Width, bitmapData.Height,
+            bitmap.HorizontalResolution, bitmap.VerticalResolution,
+            PixelFormats.Bgr24, null,
+            bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+        bitmap.UnlockBits(bitmapData);
+
+        return bitmapSource;
+    }
+}
---
+++ TGAViewer/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using TGAViewer.MVVM.Model;
+
+namespace TGAViewer.MVVM.ViewModel;
+
+public class MainWindowViewModel {
+    private string? _path;
+
+    private string Path {
+        get {
+            if (_path == null) {
+                var args = Environment.GetCommandLineArgs();
+                if (args.Length <= 1) {
+                    MessageBox.Show("Please specify path to image!");
+                    Environment.Exit(0);
+                }
+                return args[1];
+            }
+
+            return _path;
+        }
+    }
+
+    private BitmapSource? _image;
+
+    public BitmapSource Image {
+        get {
+            if (_image == null)
+                _image = TgaLoader.LoadAndDisplayTgaImage(Path);
+            return _image;
+        }
+    }
+
+    public double MinHeight => Image.Height / 10;
+    public double MinWidth => Image.Width / 10;
+}
